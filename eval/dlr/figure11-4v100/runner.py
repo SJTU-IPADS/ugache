0c540b6cfd1ebda7c738012720dcd2362fe8ffeb
Yiwen Zhang <Bessss-zyw@sjtu.edu.cn>
2023-07-23 22:30:03 +0800

    [chore] add 4v100 figure 11 eval

---
+++ eval/dlr/figure11-4v100/runner.py
@@ -0,0 +1,56 @@
+import os, sys, copy
+sys.path.append(os.getcwd()+'/../common')
+from runner_helper import System, Model, Dataset, CachePolicy, RunConfig, ConfigList, RandomDataset, percent_gen
+
+do_mock = False
+durable_log = True
+
+cur_common_base = (ConfigList()
+  .override('root_path', ['/datasets_dlr/'])
+  .override('epoch', [3])
+  .override('gpu_num', [4])
+  .override('logdir', ['run-logs-paper'])
+  .override('confdir', ['run-configs'])
+  .override('profile_level', [3])
+  .override('multi_gpu', [True])
+  .override('coll_cache_scale', [16])
+  .override('model', [Model.dlrm, Model.dcn,])
+  .override('system', [System.collcache])
+  .override('global_batch_size', [32768])
+  .override('plain_dense_model', [True])
+  .override('mock_embedding', [True])
+  .override('random_request', [False])
+  .override('cache_percent', [0.02])
+  .override('custom_env', ['SAMGRAPH_EMPTY_FEAT=24'])
+  )
+
+cfg_list_collector = ConfigList.Empty()
+
+'''
+DLRM && DCN
+'''
+cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.criteo_tb]))
+cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.syn]))
+
+cfg_list_collector.hyper_override(
+  ['coll_cache_policy', 'coll_cache_no_group', 'coll_cache_concurrent_link', 'sok_use_hashtable'], 
+  [
+    [CachePolicy.coll_cache_asymm_link, '', 'MPSPhase', None],
+    [CachePolicy.hps, '', '', None],
+    [CachePolicy.sok, '', '', True],
+  ])
+
+if __name__ == '__main__':
+  from sys import argv
+  retry = False
+  fail_only = False
+  for arg in argv[1:]:
+    if arg == '-m' or arg == '--mock':
+      do_mock = True
+    elif arg == '-i' or arg == '--interactive':
+      durable_log = False
+    elif arg == '-r' or arg == '--retry':
+      retry = True
+    elif arg == '-f' or arg == '--fail':
+      fail_only = True
+  cfg_list_collector.run(do_mock, durable_log, retry=retry, fail_only=fail_only)
