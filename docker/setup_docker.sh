fbe7a8613def19b8ab9c7878393c6cefe84ed673
Yiwen Zhang <Bessss-zyw@sjtu.edu.cn>
2023-07-23 18:43:58 +0800

    [chore] minor update in setup_docker.sh, rm the unnecessary file

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -3,7 +3,8 @@ git clone --branch sosp23ae-sxn-dev git@ipads.se.sjtu.edu.cn:gnn/collcachelib.gi
 
 wget https://packages.gurobi.com/9.5/gurobi9.5.1_linux64.tar.gz && \
   tar xf gurobi9.5.1_linux64.tar.gz && \
-  mv gurobi951 /opt/gurobi-install
+  mv gurobi951 /opt/gurobi-install && \
+  rm gurobi9.5.1_linux64.tar.gz
 
 cd /ugache && \
   mkdir build && cd build && \
