c4c119fc7a5fecf7359ee07d24ad2c92fbb9a9e2
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2023-07-27 14:22:35 +0800

    minor refine of env script

--- docker/setup_docker.gnn.sh
+++ docker/setup_docker.gnn.sh
@@ -1,6 +1,6 @@
 git clone --depth 1 --branch 0.9.1 --recursive https://github.com/dmlc/dgl.git /dgl
-git clone --branch sxn-dev git@ipads.se.sjtu.edu.cn:gnn/wholegraph.git /wholegraph
-git clone --branch coll-cache-dev git@ipads.se.sjtu.edu.cn:gnn/samgraph.git /gnnlab
+git clone --branch sosp23ae https://github.com/Jeffery-Song/wholegraph.git /wholegraph
+git clone --branch sosp23ae https://github.com/SJTU-IPADS/gnnlab.git /gnnlab
 cd /gnnlab && git submodule update --init --recursive 3rdparty/parallel-hashmap 3rdparty/CLI11 
 
 cd /dgl && \
@@ -9,11 +9,13 @@ cd /dgl && \
   cd python && python setup.py build && python setup.py install
 
 cd /wholegraph && \
+  git checkout 916cf7a9 && \
   mkdir -p build && cd build && \
   cmake .. && \
   make -j
 
 cd /gnnlab && \
+  git checkout 5f0abe65 && \
   python setup.py build && python setup.py install
 
 cd /ugache/python && \
--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -1,5 +1,5 @@
 pip install ninja numba
-git clone --branch sosp23ae-sxn-dev git@ipads.se.sjtu.edu.cn:gnn/collcachelib.git /ugache
+git clone --branch sosp23ae https://github.com/SJTU-IPADS/ugache-artifacts.git /ugache
 
 wget https://packages.gurobi.com/9.5/gurobi9.5.1_linux64.tar.gz && \
   tar xf gurobi9.5.1_linux64.tar.gz && \
