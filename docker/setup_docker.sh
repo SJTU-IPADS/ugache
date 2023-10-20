318e5fe730a07250ae960dd37328c83616cc154a
sxn <xiaoniu.sxn@outlook.com>
2023-10-20 09:04:48 +0000

    update gurobi

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -1,10 +1,10 @@
 pip install ninja numba
 git clone --branch sosp23ae https://github.com/SJTU-IPADS/ugache-artifacts.git /ugache
 
-wget https://packages.gurobi.com/9.5/gurobi9.5.1_linux64.tar.gz && \
-  tar xf gurobi9.5.1_linux64.tar.gz && \
-  mv gurobi951 /opt/gurobi-install && \
-  rm gurobi9.5.1_linux64.tar.gz
+wget https://packages.gurobi.com/10.0/gurobi10.0.3_linux64.tar.gz && \
+  tar xf gurobi10.0.3_linux64.tar.gz && \
+  mv gurobi1003 /opt/gurobi-install && \
+  rm gurobi10.0.3_linux64.tar.gz
 
 cd /ugache && \
   mkdir build && cd build && \
